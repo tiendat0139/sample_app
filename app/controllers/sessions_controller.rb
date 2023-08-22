8eabce0f809aa6ac32579db2ba46877f71c5d978
tiendat0139 <dattien090909@gmail.com>
2023-08-22 13:25:43 +0700

    Chapter 8

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by email: params.dig(:session, :email)&.downcase
+    if user&.authenticate(params.dig(:session, :password))
+      reset_session
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = t(".invalid")
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url, status: :see_other
+  end
+end

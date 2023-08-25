6c8b93b9f7edeb9ecaa983f9aa74ef34769649df
tiendat0139 <dattien090909@gmail.com>
2023-08-25 11:59:15 +0700

    Chapter 11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   before_save{email.downcase!}
+  before_create :create_activation_digest
   VALID_EMAIL_REGEX = Settings.regexes.email
   validates :name,  presence: true,
                     length: {maximum: Settings.digits.length_50}
@@ -39,13 +40,28 @@ class User < ApplicationRecord
     remember_digest || remember
   end
 
-  def authenticated? remember_token
-    return false unless remember_digest
+  # Returns true if the given token matches the digest.
+  def authenticated? attribute, token
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
 
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
     update_column :remember_digest, nil
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
+  def activate
+    update_columns activated: true, activated_at: Time.zone.now
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 end
